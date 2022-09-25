bda73208d9e4544c2e04b948664156dbfd6e6039
Oleg-Gavrilyuk <gavrilyuk161090@yandex.ru>
2022-09-25 13:13:03 +0300

    1

---
+++ main.py
@@ -0,0 +1,7 @@
+from application.salary import calculate_salary
+from application.db.people import get_employees
+from datetime import datetime, date, time
+if __name__ == '__main__':
+    calculate_salary()
+    get_employees()
+    print(datetime.now())
